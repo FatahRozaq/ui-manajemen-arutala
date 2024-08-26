aa58f747b8ee6205aace949675b1adfcbb924a8f
FatahRozaq <fatah.rozaq20@gmail.com>
2024-08-26 10:24:03 +0700

    Update pendaftar

--- app/Models/Pendaftar.php
+++ app/Models/Pendaftar.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Pendaftar extends Authenticatable
 {
@@ -12,7 +13,7 @@ class Pendaftar extends Authenticatable
 
     protected $table = 'pendaftar';
 
-    protected $primaryKey = 'id_pendaftar'; 
+    protected $primaryKey = 'id_pendaftar';
 
     protected $fillable = [
         'nama',
