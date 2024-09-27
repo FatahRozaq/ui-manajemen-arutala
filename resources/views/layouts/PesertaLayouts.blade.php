2d7b55a632b1c3d5e94cd2095f2f33d406a7e974
gianvilcanpatra <gianvilcanpatra@gmail.com>
2024-09-27 17:05:07 +0700

    update

--- resources/views/layouts/PesertaLayouts.blade.php
+++ resources/views/layouts/PesertaLayouts.blade.php
@@ -35,11 +35,77 @@
         <nav class="header-nav ms-auto">
             <ul class="d-flex align-items-center">
                 <li class="nav-item dropdown pe-3">
+                    <li class="nav-item dropdown">
+
+                        <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                          <i class="bi bi-chat-left-text" style="font-size: 16px"></i>
+                          <span class="badge bg-success badge-number" style="font-size: 10px; margin-top:10px; margin-right:5px;">3</span>
+                        </a><!-- End Messages Icon -->
+              
+                        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+                          <li class="dropdown-header">
+                            You have 3 new messages
+                            <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                          </li>
+                          <li>
+                            <hr class="dropdown-divider">
+                          </li>
+              
+                          {{-- <li class="message-item">
+                            <a href="#">
+                              <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
+                              <div>
+                                <h4>Maria Hudson</h4>
+                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                <p>4 hrs. ago</p>
+                              </div>
+                            </a>
+                          </li>
+                          <li>
+                            <hr class="dropdown-divider">
+                          </li>
+              
+                          <li class="message-item">
+                            <a href="#">
+                              <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle">
+                              <div>
+                                <h4>Anna Nelson</h4>
+                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                <p>6 hrs. ago</p>
+                              </div>
+                            </a>
+                          </li>
+                          <li>
+                            <hr class="dropdown-divider">
+                          </li>
+              
+                          <li class="message-item">
+                            <a href="#">
+                              <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle">
+                              <div>
+                                <h4>David Muldon</h4>
+                                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
+                                <p>8 hrs. ago</p>
+                              </div>
+                            </a>
+                          </li> --}}
+                          <li>
+                            <hr class="dropdown-divider">
+                          </li>
+              
+                          <li class="dropdown-footer">
+                            <a href="#">Show all messages</a>
+                          </li>
+              
+                        </ul><!-- End Messages Dropdown Items -->
+              
+                      </li><!-- End Messages Nav -->
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
                         <span id="navbarUserName" class="d-none d-md-block dropdown-toggle ps-2" style="font-size: 14px;">Peserta</span>
                         <i class="fa-solid fa-circle-user" style="font-size: 25px; margin-left:20px; margin-right:10px;"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                        
                         <li class="dropdown-header">
                             <h6 id="navbarProfileName">Peserta</h6>
                             <span>Peserta</span>
