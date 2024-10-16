19f2db899bd24c8e5e90a3f8a3a7002128f57093
gianvilcanpatra <gianvilcanpatra@gmail.com>
2024-10-16 08:35:35 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -357,8 +357,8 @@ fetchTotalPeserta(); // Default fetch without any filters
                 <select id="year-tren" class="form-select">
                   <option value="2024">2024</option>
                   <option value="2023">2023</option>
-                  <option value="2022">2022</option>
-                  <option value="2021">2021</option>
+                  {{-- <option value="2022">2022</option>
+                  <option value="2021">2021</option> --}}
                 </select>
               </li>
               <li class="dropdown-item d-flex justify-content-between align-items-center">
