129d5c900344ab0b9260d39a138b6c6bfd253653
FatahRozaq <fatah.rozaq20@gmail.com>
2024-12-16 11:32:43 +0700

    Update sertif

--- app/Http/Controllers/Api/ApiSertifikatController.php
+++ app/Http/Controllers/Api/ApiSertifikatController.php
@@ -489,9 +489,9 @@ class ApiSertifikatController extends Controller
                 $urutan = str_pad($index + 1, 3, '0', STR_PAD_LEFT);
                 $url = "https://atms.arutalalab.net/sertifikat/$certificateNumber.$urutan";
                 $namaPeserta = $pendaftaran->pendaftar->nama;
-                $qrCodeName = "$namaPeserta.$certificateNumber.$urutan.png";
+                $qrCodeName = "$namaPeserta.$certificateNumber.$urutan.svg";
 
-                $qrImage = QrCode::format('png')
+                $qrImage = QrCode::format('svg')
                     ->size(300)
                     ->generate($url);
 
